c4f183fd784f1247be9d4ec8ece2db3f0c62bad4
hsmanski <hsmanski@gmail.com>
2022-03-20 21:55:03 -0700

    Repos api added and displayed on site

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 const overview = document.querySelector(".overview");
 // Git hub username
 const username = "hsmanski";
+// Select ul to display repos list
+const reposList = document.querySelector(".repo-list");
 
 // Fetch API Github profile
 const githubUserInfo = async function () {
@@ -12,6 +14,7 @@ const githubUserInfo = async function () {
 };
 githubUserInfo();
 
+// Display User Information
 const showUserInfo = function (data) {
   const div = document.createElement("div");
   div.classList.add("user-info");
@@ -27,4 +30,25 @@ const showUserInfo = function (data) {
     </div>
   `;
   overview.append(div);
+  fetchRepos();
+};
+
+// Fetch Github Repos
+const fetchRepos = async function () {
+  const gitRepos = await fetch(
+    `https://api.github.com/users/${username}/repos?sort=update&per_page=100`
+  );
+  const repoData = await gitRepos.json();
+  console.log(repoData);
+  displayRepos(repoData);
+};
+
+// Display Github Repos
+const displayRepos = function (repos) {
+  for (const repo of repos) {
+    const repoItem = document.createElement("li");
+    repoItem.classList.add("repo");
+    repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+    reposList.append(repoItem);
+  }
 };
